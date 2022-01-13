938152d067fb9ab0331bbce655fc0f7959421ef2
guvanch07 <guvanchamanov777@gmail.com>
2022-01-13 12:23:20 +0300

    need to test

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flash_chat/app.dart';
+import 'package:flash_chat/screens/select_user_screen.dart';
+import 'package:flash_chat/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
+
+void main() {
+  final client = StreamChatClient(streamKey);
+
+  runApp(
+    MyApp(
+      client: client,
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.client,
+  }) : super(key: key);
+
+  final StreamChatClient client;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: ThemeMode.dark,
+      title: 'Chatter',
+      builder: (context, child) {
+        return StreamChatCore(
+          client: client,
+          child: ChannelsBloc(
+            child: UsersBloc(
+              child: child!,
+            ),
+          ),
+        );
+      },
+      home: const SelectUserScreen(),
+    );
+  }
+}
